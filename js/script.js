4c532d30355eae27352c869124a3076a6ee769f8
RobertoFlores12 <robertoflores20a@gmail.com>
2023-06-05 21:16:18 -0600

    primer comentario

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const  textArea = document.querySelector(".tex-area");
+const  mensaje = document.querySelector(".mensaje");
+
+
+// letra "e" es convertida a "entre"
+// letra "i" es convertida a "imes"
+// letra "o" es convertida a "ober"
+// letra "a" es convertida a "ai"
+// letra "u" es convertida a "ufat"
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+}
+
+function encriptar(stringEncriptado){
+    let matrizCodigo = [["e","enter"],["i","imes"],["o","ober"],["a","ai"],["u","ufat"]]
+    stringEncriptado = stringEncriptado.toLowerCase()
+
+    for(let i =0; i<matrizCodigo.length;i++){
+        if(stringEncriptado.includes(matrizCodigo[i][0])){
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptado
+}
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+}
+
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e","enter"],["i","imes"],["o","ober"],["a","ai"],["u","ufat"]]
+    stringDesencriptada = stringDesencriptada.toLowerCase()
+
+    for(let i =0; i<matrizCodigo.length;i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptada
+}
